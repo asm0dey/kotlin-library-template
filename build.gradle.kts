1ab15e03b08a4296801dd8ecdfff10e47f195aea
asm0dey <pavel.finkelshtein@gmail.com>
2022-11-28 20:46:22 +0100

    Adds Kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("jvm") version "1.7.21"
     id("org.jetbrains.dokka") version "1.7.20"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.12.1"
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
 }
 
 group = "org.example"
@@ -17,6 +18,7 @@ repositories {
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
 }
 
 tasks.withType<DokkaTaskPartial>().configureEach {
@@ -25,4 +27,8 @@ tasks.withType<DokkaTaskPartial>().configureEach {
 
 tasks.withType<DokkaTask>().configureEach {
     dokkaSourceSets.configureEach { reportUndocumented.set(true) }
+}
+
+koverMerged {
+    enable()
 }
